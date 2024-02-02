28825d53d4a072a3d8bd3ca69f3cb6ea76182bb5
luisa <luisags400@gmail.com>
2024-02-02 11:04:41 -0300

    Category Findall conectado

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,47 +1,42 @@
+const db = require("../db");
 const CategoryController = {
-    findAll(req, res) {
+    async findAll(req, res) {
+        try {
+            const category = await db.query("SELECT * FROM category");
+            res.json(category.rows);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
 
-         /**
-       * Aqui entraria a regra de pesistência do banco de dados
-       */
-
-        res.json([
-            {
-                id: 1, nome: 'Filmes A', description: 'Essa categoria traz filmes com a letra A',
-            },
-            {
-                id: 2, nome: 'Filmes B', description: 'Essa categoria traz filmes com a letra B',
-            },
-        ]);
+        }
     },
     find(req, res) {
         const { id } = req.params;
-         /**
-       * Aqui entraria a regra de pesistência do banco de dados
-       */
+        /**
+      * Aqui entraria a regra de pesistência do banco de dados
+      */
         res.json({
-             id: id,
-             nome: 'Filmes A',
-             description: 'Essa categoria traz filmes com a letra A',
+            id: id,
+            nome: 'Filmes A',
+            description: 'Essa categoria traz filmes com a letra A',
         });
     },
-    create(req, res) { 
-        const { name, description }= req.body;
-      /**
-       * Aqui entraria a regra de pesistência do banco de dados
-       */
+    create(req, res) {
+        const { name, description } = req.body;
+        /**
+         * Aqui entraria a regra de pesistência do banco de dados
+         */
         res.status(201).json({
             id: Number.MAX_SAFE_INTEGER,
             name: name,
             description: description,
-       });
+        });
 
     },
-    delete(req, res) { 
-        const { id }= req.params;
-         /**
-       * Aqui entraria a regra de pesistência do banco de dados
-       */
+    delete(req, res) {
+        const { id } = req.params;
+        /**
+      * Aqui entraria a regra de pesistência do banco de dados
+      */
         res.status(204).json();
     },
 };
