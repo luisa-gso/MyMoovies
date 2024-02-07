363f451e828e5e9eac53cd926991d2126d4ae988
luisa <luisags400@gmail.com>
2024-02-07 09:11:37 -0300

    atualização

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser =  require ("body-parser");
+const cors = require("cors");
 const categoryRoutes = require ("./src/routes/category");
 const mooviesRoutes = require ("./src/routes/moovies");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+app.use(cors());
+const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 
